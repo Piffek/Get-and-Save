624535ebe77b3df3f16a351857bc6b7ed5e016bf
Piffek <patrykpiwko123412@gmail.com>
2017-12-15 22:34:18 +0100

    ref

--- src/File.php
+++ src/File.php
@@ -19,24 +19,43 @@ class File implements FileInterface
         $this->location = $location;
     }
 
+    /**
+     * File to taken content.
+     *
+     * @param string $name
+     */
     public function fromFile(string $name)
     {
         $this->from = $name;
     }
 
+    /**
+     * When store new content.
+     *
+     * @param string $name
+     */
     public function toFile(string $name)
     {
         $this->to = $name;
     }
 
-    public function getContentFromFile()
+    /**
+     * Get content any file.
+     *
+     * @return \File
+     */
+    public function getContentFromFile() : File
     {
-        $locationOfFile = '%s/%s';
-        $content = sprintf($locationOfFile, $this->location->getPath(), $this->from);
+        $content = $this->makeLocationToFile($this->location->getPath(), $this->from);
 
         return $this->explodeContentToArray(file_get_contents($content));
     }
 
+    /**
+     * Explode content any file to array and pass it to saveNewContentToFile method.
+     *
+     * @param String $content
+     */
     public function explodeContentToArray(String $content)
     {
         $param = '';
@@ -49,7 +68,13 @@ class File implements FileInterface
         $this->saveNewContentToFile($param);
     }
 
-    public function operation(String $line) : string
+    /**
+     * Explode one line and check activities. Bind result to one string.
+     *
+     * @param String $line
+     * @return string
+     */
+    public function operation(String $line): string
     {
         $paramInLine = explode(' ', $line);
 
@@ -84,12 +109,29 @@ class File implements FileInterface
         return $newContent;
     }
 
+    /**
+     * Save new content to file.
+     *
+     * @param string $param
+     */
     public function saveNewContentToFile(string $param)
     {
+        $contentToSave = $this->makeLocationToFile($this->location->getPath(), $this->to);
+
+        file_put_contents($contentToSave, $param);
+    }
+
+    /**
+     * make file location.
+     *
+     * @param string $path
+     * @param string $direction TO/FROM
+     * @return string
+     */
+    private function makeLocationToFile(string $path, string $direction) : string
+    {
         $locationOfFile = '%s/%s';
-        $content = sprintf($locationOfFile, $this->location->getPath(), $this->to);
 
-
-        file_put_contents($content, $param);
+        return sprintf($locationOfFile, $path, $direction);
     }
 }
--- src/Location.php
+++ src/Location.php
@@ -4,11 +4,20 @@ class Location
 {
     private $path;
 
+    /**
+     * Set path to file.
+     *
+     * @param string $name
+     */
     public function setPath(string $name)
     {
         $this->path = $name;
     }
 
+    /**
+     * Get path to file.
+     * @return mixed
+     */
     public function getPath()
     {
         return $this->path;
